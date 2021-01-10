334bc83f69a5e3892930cb3c4505df7e86aac599
Felipe Alencar <fsalencar@outlook.com>
2021-01-10 14:49:39 -0300

    Update settings.py

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['case-gestao-clientes.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['case-gestao-clientes.herokuapp.com','case-gestaoclientes-dev.herokuapp.com', 'localhost']
 
 # Application definition
 
